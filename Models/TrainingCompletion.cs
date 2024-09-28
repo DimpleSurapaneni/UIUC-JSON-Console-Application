9ec8df5acc851e526446476f1deefc7624c73054
DimpleSurapaneni <dsurapaneni99@gmail.com>
2024-09-28 14:09:47 -0400

    Add project files.

---
+++ Models/TrainingCompletion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIUC_JSON_Console_Application.Models
+{
+    public class TrainingCompletion
+    {
+        public string? Name { get; set; }
+        public DateTime Timestamp { get; set; }
+        public DateTime? Expires { get; set; }
+    }
+}
